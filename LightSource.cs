b8ef6d02dd4a0d73ab80ca513a3e80628d01509c
VladSharobaicko <61946717+VladSharobaicko@users.noreply.github.com>
2020-03-09 22:13:37 +0300

    Add basicOopticSystem

--- LightSource.cs
+++ LightSource.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 
@@ -6,12 +7,7 @@ namespace Optics
 {
     public class LightSource
     {
-        private Vector2 position;
-        public Vector2 Position { get => position; private set => position = value; }
-
-        public int NumberOfBeams = 10;
-
-        public List<Vector2> beamsDirections;
+        public Vector2 Position { get; private set;}
 
         public float RayRange = 100f;
 
@@ -19,8 +15,26 @@ namespace Optics
         public LightSource(Vector2 position, List<Vector2> beamsDirections, float rayRange)
         {
             Position = position;
-            this.beamsDirections = beamsDirections;
             RayRange = rayRange;
+        }
+
+        /// <summary>
+        /// Evenly spreads N of rays betwen a and b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        public List<Ray> CastRays(Vector2 a,Vector2 b, int N)
+        {
+            var rays = new List<Ray>();
+
+            for (int i = 1; i < N+1; i++)
+            {
+                var v = Vector2.Lerp(a, b, (float)i / (N + 1f));
+                rays.Add(new Ray(Position,v));
+            }
+            return rays;
         }
     }
 }
---
+++ OpticSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Optics
+{
+    class OpticSystem
+    {
+        LightSource lightSource;
+        List<Lens> lenses;
+        List<Ray> rays;
+
+        public OpticSystem(LightSource lightSource, List<Lens> lenses)
+        {
+            this.lightSource = lightSource;
+            this.lenses = lenses;
+            rays = new List<Ray>();
+        }
+    }
+}
